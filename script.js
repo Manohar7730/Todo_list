61b131ad552de07057f17ae175ec085b49d80654
Manohar <manohar20century@gmail.com>
2023-07-02 11:05:42 +0530

    Added function and some code to add the task with li and span using javascript dom manupulation

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const addBtn = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
 
 addBtn.addEventListener('click',function(){
     if(taskInput.value.trim() !== ""){
+        addTask(taskInput.value.trim());
         console.log("Task added")
         taskInput.value = ""
     }
-    console.log("Add Task")
 })
+
+function addTask(taskText){
+    const li=document.createElement("li");
+    li.classList.add("task-item");
+
+    const span=document.createElement('span');
+    span.classList.add("task-text");
+    span.textContent = taskText;
+    li.appendChild(span);
+
+    taskList.appendChild(li);
+    console.log("Task Created");
+}
