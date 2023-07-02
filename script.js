75a1a546faa0240ea15671300d3bdd67884af04d
Manohar <manohar20century@gmail.com>
2023-07-02 10:49:16 +0530

    if task input is not empty , we will add the task and after adding we will reset the input form

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
 
 addBtn.addEventListener('click',function(){
+    if(taskInput.value.trim() !== ""){
+        console.log("Task added")
+        taskInput.value = ""
+    }
     console.log("Add Task")
 })
